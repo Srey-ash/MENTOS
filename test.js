0492dcfc1a23cb8f906c71b99aa589cf89f0c60a
Srey-ash <123293819+Srey-ash@users.noreply.github.com>
2023-05-03 00:29:02 +0530

    Update test.js

--- test.js
+++ test.js
@@ -1,28 +1,3 @@
-function auditscore(questions) {
-  var total = 0;
-  for (var i = 0; i < questions.elements.length; i++) {
-      if (questions.elements[i].type == 'radio' && questions.elements[i].checked) {
-          total += parseFloat(questions.elements[i].value)
-      }
-  }
-  if (total < 17) {
-    window.alert("Please answer all questions!")
-}
-if (total >= 17 && total < 28) {
-    document.write('Subthreshold depression symptoms')
-}
-if (total >= 28 && total < 30) {
-  document.write('Possible major depressive episode')
-}
-if (total >= 30 && total < 45) {
-  document.write('Probable major depressive episode')
-}
-if (total >= 45) {
-  document.write('Meets criteria for Major depressive episode')
-}
-  return total
-}
-
 function cesdscore(questions) {
   var total = 0;
   for (var i = 0; i < questions.elements.length; i++) {
@@ -31,7 +6,7 @@ function cesdscore(questions) {
       }
   }
   if (total < 17) {
-    window.alert("Please answer all questions!")
+    document.write("You are fine!! Stay Happy!!")
 }
 if (total >= 17 && total < 28) {
     document.write('Subthreshold depression symptoms')
@@ -48,52 +23,3 @@ if (total >= 45) {
   return total
 }
 
-function cesdcscore(questions) {
-  var total = 0;
-  for (var i = 0; i < questions.elements.length; i++) {
-      if (questions.elements[i].type == 'radio' && questions.elements[i].checked) {
-          total += parseFloat(questions.elements[i].value)
-      }
-  }
-  if (total < 17) {
-    window.alert("Please answer all questions!")
-}
-if (total >= 17 && total < 28) {
-    document.write('Subthreshold depression symptoms')
-}
-if (total >= 28 && total < 30) {
-  document.write('Possible major depressive episode')
-}
-if (total >= 30 && total < 45) {
-  document.write('Probable major depressive episode')
-}
-if (total >= 45) {
-  document.write('Meets criteria for Major depressive episode')
-}
-  return total
-}
-
-function ptsdscore(questions) {
-  var total = 0;
-  for (var i = 0; i < questions.elements.length; i++) {
-      if (questions.elements[i].type == 'radio' && questions.elements[i].checked) {
-          total += parseFloat(questions.elements[i].value)
-      }
-  }
-  if (total < 17) {
-      window.alert("Please answer all questions!")
-  }
-  if (total >= 17 && total < 28) {
-      document.write('Subthreshold depression symptoms')
-  }
-  if (total >= 28 && total < 30) {
-    document.write('Possible major depressive episode')
-  }
-  if (total >= 30 && total < 45) {
-    document.write('Probable major depressive episode')
-  }
-  if (total >= 45) {
-    document.write('Meets criteria for Major depressive episode')
-  }
-  return total
-}
